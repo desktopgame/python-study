d6cffffea227a08d1f3ef924cc83b4d2d2b663b1
Koya <koya0306@outlook.jp>
2020-02-08 10:26:11 +0900

    pygameを試してみるためのプロジェクト追加

---
+++ gameTest/app/main.py
@@ -0,0 +1,12 @@
+import pygame
+import sys
+
+pygame.init()
+SURFACE = pygame.display.set_mode((1280, 720))
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+    pygame.display.update()
