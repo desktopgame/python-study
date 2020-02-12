42d9b38ab671c433e7639843561feb4446421fd9
desktopgame <gnndr36@gmail.com>
2020-02-12 22:02:52 +0900

    お絵かきアプリ実装

--- gameTest/app/main.py
+++ gameTest/app/main.py
@@ -1,21 +1,26 @@
 import pygame
 import sys
-import scene
-
+import scene 
 from title import TitleScene
 
+
 pygame.init()
-surface = pygame.display.set_mode((1280, 720))
+surface = pygame.display.set_mode((32*20, 32*15))
 
 mgr = scene.SceneManager(surface)
 mgr.add('title', TitleScene())
 mgr.bind('title')
 
-while True:
+run = True
+
+while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
-            sys.exit()
+            run = False
+            break
+    if not run:
+        break
     mgr.update()
     mgr.draw()
     pygame.display.update()
--- gameTest/app/title.py
+++ gameTest/app/title.py
@@ -1,22 +1,41 @@
 import scene
 import pygame
-from pygame.draw import draw as pydraw
+import pygame.draw as pydraw
+import pygame.mouse as pymouse
 
 
 class TitleScene(scene.Scene):
 
     def show(self):
-        print('show')
+        w, h = pygame.display.get_surface().get_size()
+        self.table = [
+            [0 for x in range(int(w / 32))] for y in range(int(h / 32))
+        ]
 
     def hide(self):
         print('hide')
 
     def update(self):
-        pass
+        w, h = pygame.display.get_surface().get_size()
+        btn1, btn2, btn3 = pymouse.get_pressed()
+        mx, my = pymouse.get_pos()
+        cx, cy = int(mx / 32), int(my / 32)
+        if btn1:
+            self.table[cy][cx] = 1
+        elif btn3:
+            self.table[cy][cx] = 0
 
     def draw(self, surface: pygame.Surface):
-        pydraw.rect(surface, (78, 203, 245), (0, 0, 250, 500), 5)
-        surface.fill((0, 255, 0))
+        w, h = surface.get_size()
+        surface.fill((0, 0, 0))
+        for i in range(int(w / 32)):
+            for j in range(int(h / 32)):
+                if self.table[j][i] == 1:
+                    pydraw.rect(
+                        surface,
+                        pygame.Color(0, 255, 0, 255),
+                        pygame.Rect((32 * i), (32 * j), 32, 32)
+                    )
 
     @property
     def is_finished(self):
