68c47f373c11f9ece36f0dc4f6fc44aaa3d5e754
desktopgame <gnndr36@gmail.com>
2020-02-21 21:57:08 +0900

    スロットの描画/更新システム実装

--- pyxelTest/app/main.py
+++ pyxelTest/app/main.py
@@ -17,13 +17,10 @@ class App:
 
     def draw(self):
         pyxel.cls(0)
-        cell_size = 20
-        for col in range(16):
-            pyxel.rect(
-                int(col % 10) * cell_size,
-                int(col / 10) * cell_size,
-                cell_size, cell_size, col
-            )
+        for i in range(self.stage.row_count):
+            for j in range(self.stage.column_count):
+                self.stage[i][j].update()
+                self.stage[i][j].draw()
 
 
 App()
--- pyxelTest/stage/slot.py
+++ pyxelTest/stage/slot.py
@@ -1,6 +1,12 @@
 class Slot:
     def __init__(self, point):
         self.__point = point
+
+    def update(self):
+        pass
+
+    def draw(self):
+        pass
     
     @property
     def point(self):
